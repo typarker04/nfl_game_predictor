d13d8cf078e1faaa4b213f049386ac94a15f4015
typarker04 <trparker@wisc.edu>
2026-01-15 15:49:48 -0500

    pages and more outputs

---
+++ pages/2_about.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import nflreadpy as nfl
+
+
+st.set_page_config(page_title="Season Stats", page_icon="🌍")
+
+st.markdown("# Mapping Demo")
+st.sidebar.header("Mapping Demo")
+st.write(
+    """This demo shows how to use
+[`st.pydeck_chart`](https://docs.streamlit.io/develop/api-reference/charts/st.pydeck_chart)
+to display geospatial data."""
+)
