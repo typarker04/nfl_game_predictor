6d194a2bce8289c339631957d7a828a082da881c
typarker04 <trparker@wisc.edu>
2026-01-19 15:18:48 -0500

    week 21 updates

---
+++ pages/3_about.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+from pathlib import Path
+
+st.set_page_config(
+    page_title="About",
+    layout="wide"
+)
